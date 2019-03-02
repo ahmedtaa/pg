f33a3f65f4329916a9a62b7ec7ad462636c6782f
ahmedtaa <a.alrakhawy@gmail.com>
2019-03-02 04:46:02 +0200

    added travis deployment solving error 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-          CCOOOOLLcool
+          COOOOOOOOOOOL
           
           </p>
           <a
